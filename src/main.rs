c643e7396cfb771c20b47411417c490f7afc4cd0
Celine-Coralie-Eni <celinecoralie@icloud.com>
2025-02-25 15:37:38 +0100

    "Update: addded the  Dockerfile and entrypoint.sh file"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use number::fibonacci;
+
 fn main() {
-    println!("Hello, world!");
-}
+    print!("Hello world");
+        let n = 29; 
+        println!("Fibonacci numbers up to {}:", n);
+        for i in 0..n {
+            println!("{}", fibonacci(i));
+        }
+    
+    // if n = f32 {
+    //     println!("invalid input");
+    }
+    
+
+
+mod number;
