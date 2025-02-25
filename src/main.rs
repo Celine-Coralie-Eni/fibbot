0d4646defb11c99296a60e839929e9472f3a3049
celine <celinecoralie@icloud.com>
2025-02-25 17:05:59 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use number::fibonacci;
-
 fn main() {
     print!("Hello world");
-        let n = 29; 
-        println!("Fibonacci numbers up to {}:", n);
-        for i in 0..n {
-            println!("{}", fibonacci(i));
-        }
-    
-    // if n = f32 {
-    //     println!("invalid input");
     }
-    
-
-
-mod number;
