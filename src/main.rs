52d9a2a161dcb761179d623dd48d25c1fc18d49b
celine <celinecoralie@icloud.com>
2025-02-26 09:43:12 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
 fn main() {
     print!("Hello world");
-    
-    match Cli::from_args() {
-        Ok(cli) => {
-            if cli.enable_fib {
-                println!("Generating Fibonacci sequence up to {}", cli.max_threshold);
-                // Implement your Fibonacci sequence generation logic here
-                let mut a = 0;
-                let mut b = 1;
-                while a <= cli.max_threshold {
-                    println!("{}", a);
-                    let next = a + b;
-                    a = b;
-                    b = next;
-                }
-            } else {
-                println!("Fibonacci sequence generation is disabled");
-            }
-        }
-        Err(e) => {
-            eprintln!("Error: {}", e);
-        }
-    }
 }
     
     
