7089f1b392c05b8ca3c7ab4d34e08975b6680301
celine <celinecoralie@icloud.com>
2025-03-02 00:41:23 +0100

    Update get_pr.rs

--- src/get_pr.rs
+++ src/get_pr.rs
@@ -1,11 +1,10 @@
 use crate::extract_num::extract_numbers;
 
-pub async fn get_pr_body(pr_number: u64) -> Vec<i32>{
+pub async fn get_pr_body(pr_number: u64) -> Vec<u32>{
 
     let files = octocrab::instance().pulls("Celine-Coralie-Eni", "fibbot").list_files(pr_number).await;
     let files = files.unwrap().items.first().unwrap().patch.clone().unwrap();
     println!("Pull Resquest Contents:\n{}",files);
-    let nums = extract_numbers(&files.as_str().to_string());
+    let nums:Vec<u32> = extract_numbers(&files.as_str().to_string());
     nums
  }
- 
